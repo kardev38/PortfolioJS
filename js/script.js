a3bfc4c4624d1db19961ccdc85f7fb06f2ecd216
kardev38 <moujahedk83@gmail.com>
2021-10-17 13:57:08 +0200

    modif tooltip , wrap texte email

--- js/script.js
+++ js/script.js
@@ -86,6 +86,54 @@ let itemIndex=0;
 
     lightbox.addEventListener("click",function(event){
         if(event.target === lightboxClose || event.target === lightbox){
-            toggleLightbox()
+            toggleLightbox();
         }
     })
+
+    // Aside Navbar
+    const nav=document.querySelector(".nav"),
+        navList=nav.querySelectorAll("li"),
+        totalNavList=navList.length,
+        allSection=document.querySelectorAll(".section"),
+        totalSection=allSection.length;
+
+        for(let i=0; i<totalNavList; i++){
+            const a=navList[i].querySelector("a");
+            a.addEventListener("click", function(){ 
+                // remove back section Class
+                for(let i=0; i<totalSection; i++){
+                    allSection[i].classList.remove("back-section");
+                }
+
+                for(let j=0; j<totalNavList; j++){
+                    if(navList[j].querySelector("a").classList.contains("active")){
+                        // add back section Class
+                        allSection[j].classList.add("back-section");
+                    }
+                    navList[j].querySelector("a").classList.remove("active");
+                }
+                this.classList.add("active");
+
+                showSection(this);
+            })
+        }
+
+        function showSection(element){
+            for(let i=0; i<totalSection; i++){
+                allSection[i].classList.remove("active");
+            }
+            const target=element.getAttribute("href").split("#")[1];
+            document.querySelector("#"+target).classList.add("active");
+        }
+
+        const navTogglerBtn=document.querySelector(".nav-toggler"),
+            aside=document.querySelector(".aside");
+
+            navTogglerBtn.addEventListener("click",() =>{
+                asideSectionTogglerBtn();
+            })
+
+            function asideSectionTogglerBtn(){
+                aside.classList.toggle("open");
+                navTogglerBtn.classList.toggle("open");
+            }
