7acb4cfed91d08db6ae71ac2fbb0bfc503bad037
kardev <moujahedk83@gmail.com>
2021-11-12 23:22:43 +0100

    test4

--- js/script.js
+++ js/script.js
@@ -182,18 +182,4 @@ let itemIndex=0;
 
             // Script Video
 
-            // var video = document.querySelectorAll('video')
-
-            // video.forEach(play => play.addEventListener('click', () =>{
-
-            //     play.classList.toggle('active');
-
-            //     if(play.pause){
-            //         play.play();
-            //     }
-            //     else{
-            //         play.pause();
-            //         play.currentTime = 0;
-            //     }
-
-            // }));
+      
